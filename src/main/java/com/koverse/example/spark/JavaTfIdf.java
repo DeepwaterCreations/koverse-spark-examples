864aa6fe1b6a094577f67ab684ed858a62a70fbb
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-05-31 16:36:50 -0600

    Change style to placate Maven

--- src/main/java/com/koverse/example/spark/JavaTfIdf.java
+++ src/main/java/com/koverse/example/spark/JavaTfIdf.java
@@ -17,16 +17,22 @@ public class JavaTfIdf implements java.io.Serializable {
   private static final long serialVersionUID = 8741666028339586272L;
   private final String textFieldName;
   private final String tokenizationString;
-  private final Integer n;
+  private final Integer ngramSize;
   
-  public JavaTfIdf(String textFieldName, String tokenizationString, Integer n) {
+  /**
+   * A TF-IDF spark transformation for ngrams.
+   * @param textFieldName the name of the record field containing the document text
+   * @param tokenizationString the symbols to split document text on
+   * @param ngramSize the number of words to use in ngrams
+   */
+  public JavaTfIdf(String textFieldName, String tokenizationString, Integer ngramSize) {
     this.textFieldName = textFieldName;
     this.tokenizationString = tokenizationString;
-    this.n = n;
+    this.ngramSize = ngramSize;
   }
 
   /**
-   * Divides text records into lists of n adjacent words
+   * Divides text records into lists of n adjacent words.
    * @param inputRecordsRdd input RDD of SimpleRecords
    * @return a JavaRDD of lists of n-grams from the input records
    */
@@ -39,36 +45,38 @@ public class JavaTfIdf implements java.io.Serializable {
 
     // divide lists of words into ngrams
     JavaRDD<List<String>> ngrams = words.map(line -> {
-      List<String> ngram_list = Lists.newArrayList();
-      for(int i = 0; i < (line.size() - n); i++){
-        ngram_list.add(String.join(" ", line.subList(i, i+n))); 
+      List<String> ngramList = Lists.newArrayList();
+      for (int i = 0; i < (line.size() - ngramSize); i++) {
+        ngramList.add(String.join(" ", line.subList(i, i + ngramSize))); 
       }
-      return Lists.newArrayList(ngram_list);
+      return Lists.newArrayList(ngramList);
     });
 
     return ngrams;
   }
 
   /**
-   * Pairs ngrams with their term frequencies weighted by document length
-   * @param inputNgrams input RDD of lists of ngrams, where each list matches a single document
-   * @return a JavaRDD of lists of ngrams paired with their TF values for the document represented by that list
+   * Pairs ngrams with their term frequencies weighted by document length.
+   * @param inputNgrams input RDD of lists of ngrams, where each list 
+   *     matches a single document
+   * @return a JavaRDD of lists of ngrams paired with their TF values for the 
+   *     document represented by that list
    */
-  public JavaRDD<List<Tuple2<String, Integer>>> getNgramTFs(JavaRDD<List<String>> inputNgrams){
+  public JavaRDD<List<Tuple2<String, Integer>>> getNgramTFs(JavaRDD<List<String>> inputNgrams) {
     // sum up the counts for each ngram and divide by document length
-    JavaRDD<List<Tuple2<String, Integer>>> ngramCountRdd = inputNgrams.map(ngram_list -> {
+    JavaRDD<List<Tuple2<String, Integer>>> ngramCountRdd = inputNgrams.map(ngramList -> {
       // count terms by incrementing values in a hashmap
-      HashMap<String, Integer> count_map = new HashMap<String, Integer>();
-      for(String ngram : ngram_list){
-        int count = count_map.getOrDefault(ngram, 0) + 1;
-        count_map.put(ngram, count);
+      HashMap<String, Integer> countMap = new HashMap<String, Integer>();
+      for (String ngram : ngramList) {
+        int count = countMap.getOrDefault(ngram, 0) + 1;
+        countMap.put(ngram, count);
       } 
 
       // compile list of terms and pair them with counts
-      int terms_in_document = ngram_list.size();
+      int termsInDocument = ngramList.size();
       List<Tuple2<String, Integer>> pairs = Lists.newArrayList();
-      for(HashMap.Entry<String, Integer> pair : count_map.entrySet()){
-        pairs.add(new Tuple2<String, Integer>(pair.getKey(), pair.getValue() / terms_in_document)); 
+      for (HashMap.Entry<String, Integer> pair : countMap.entrySet()) {
+        pairs.add(new Tuple2<String, Integer>(pair.getKey(), pair.getValue() / termsInDocument)); 
       }
       return pairs;
     });
@@ -77,44 +85,51 @@ public class JavaTfIdf implements java.io.Serializable {
   }
 
   /**
-   * Pairs ngrams with their inverse document frequencies across the entire corpus
+   * Pairs ngrams with their inverse document frequencies across the entire corpus.
    * @param inputNgrams input RDD of lists of ngrams, where each list matches a single document
    * @return a JavaPairRDD of ngrams paired with their IDF values
    */
-  public JavaPairRDD<String, Double> getNgramIDFs(JavaRDD<List<String>> inputNgrams){
+  public JavaPairRDD<String, Double> getNgramIdfs(JavaRDD<List<String>> inputNgrams) {
 
     // generate an RDD containing the unique ngrams in the input lists
-    JavaRDD<String> ngrams = inputNgrams.flatMap(ngram_list -> {return ngram_list;}).distinct();
+    JavaRDD<String> ngrams = inputNgrams.flatMap(ngramList -> { 
+      return ngramList; 
+    })
+        .distinct();
 
-    // append to each ngram the inverse document frequency of that ngram in inputNgrams
-    JavaPairRDD<String, Double> ngram_idfs = ngrams.mapToPair(ngram -> {
-      Long ngram_count = inputNgrams.filter(ngram_list -> {return ngram_list.contains(ngram);}).count();
-      Double idf = Math.log(inputNgrams.count()/ngram_count);
+    // append to each ngram the inverse document frequency of that ngram in 
+    // inputNgrams
+    JavaPairRDD<String, Double> ngramIdfs = ngrams.mapToPair(ngram -> {
+      Long ngramCount = inputNgrams.filter(ngramList -> {
+        return ngramList.contains(ngram);
+      })
+          .count();
+      Double idf = Math.log(inputNgrams.count() / ngramCount);
       return new Tuple2<String, Double>(ngram, idf);
     });
 
-    return ngram_idfs;
+    return ngramIdfs;
   }
 
   /**
-   * Gets TF and IDF values for ngrams, multiplies them together, and puts them into a SimpleRecord
+   * Gets TF and IDF values for ngrams, multiplies them together, and puts them into a SimpleRecord.
    * @param inputRecordsRdd input RDD of SimpleRecords
    * @return a JavaRDD of SimpleRecords that have "ngram" and "tfidf" fields in each record
    */
-  public JavaRDD<SimpleRecord> getTfIdfs(JavaRDD<SimpleRecord> inputRecordsRdd){
+  public JavaRDD<SimpleRecord> getTfIdfs(JavaRDD<SimpleRecord> inputRecordsRdd) {
     // get values via other functions
-    JavaRDD<List<String>> ngram_lists = getNgrams(inputRecordsRdd);
-    JavaRDD<List<Tuple2<String, Integer>>> ngramTFs = getNgramTFs(ngram_lists);
+    JavaRDD<List<String>> ngramLists = getNgrams(inputRecordsRdd);
+    JavaRDD<List<Tuple2<String, Integer>>> ngramTFs = getNgramTFs(ngramLists);
     JavaPairRDD<String, Double> ngramIDFs = getNgramIDFs(ngram_lists);
 
     // combine tfs and idfs into a SimpleRecord RDD
-    JavaRDD<SimpleRecord> ngramTfIdfs = ngramTFs.flatMap(ngram_list -> {
+    JavaRDD<SimpleRecord> ngramTfIdfs = ngramTFs.flatMap(ngramList -> {
       List<SimpleRecord> output = Lists.newArrayList();
-      for(Tuple2<String, Integer> tfpair : ngram_list){
+      for (Tuple2<String, Integer> tfpair : ngramList) {
         SimpleRecord record = new SimpleRecord();
         String ngram = tfpair._1;
         Integer tf = tfpair._2;
-        Double idf = ngramIDFs.lookup(ngram).get(0);
+        Double idf = ngramIdfs.lookup(ngram).get(0);
         Double tfidf = tf * idf;
         record.put("ngram", ngram);
         record.put("tfidf", tfidf);
